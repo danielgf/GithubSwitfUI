ef48f12e9acae03a746b72a6fe52c85c59c15407
Daniel Griso Filho <daniel.grso.filho@gmail.com>
2023-01-26 13:38:33 -0300

    Initial Commit

---
+++ GithubSwitfUI/GithubSwitfUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSwitfUIApp.swift
+//  GithubSwitfUI
+//
+//  Created by Daniel Griso Filho on 26/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubSwitfUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
